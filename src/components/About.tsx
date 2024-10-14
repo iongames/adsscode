97357f5d92a41ba0395c39feb2e29e1f05bf0afc
ION <thegoshagames@gmail.com>
2024-10-14 10:32:23 +0300

    fix

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,55 +1,54 @@
 import { Button } from "./ui/button";
 import { motion } from "framer-motion";
 export default function About({
-	contactsRef,
+  contactsRef,
 }: {
-	contactsRef: React.MutableRefObject<HTMLDivElement | null>;
+  contactsRef: React.MutableRefObject<HTMLDivElement | null>;
 }) {
-	return (
-		<div className="@container">
-			<div className="flex justify-center items-start @[650px]:items-center  h-screen scroll-mt-[70px] mt-[30px] @[650px]:mt-0 text-white">
-				<motion.div
-					className="h-[1000px] w-[1000px] rounded-full bg-[#65646A] right-0 "
-					animate={{
-						y: -500,
-						x: 600,
-						position: "absolute",
-						scale: [0, 0, 1],
-					}}
-					transition={{ repeatType: "reverse" }}
-				/>
+  return (
+    <div className="@container">
+      <div className="flex justify-center items-start @[650px]:items-center  h-screen scroll-mt-[70px] mt-[30px] @[650px]:mt-0 text-white">
+        <motion.div
+          className="h-[1000px] w-[1000px] rounded-full bg-[#65646A] right-0"
+          animate={{
+            y: -500,
+            x: 600,
+            position: "absolute",
+            scale: [0, 0, 1],
+          }}
+          transition={{ repeatType: "reverse" }}
+        />
+        <div className="ml-[50px] mr-[50px] mt-[40px] @[800px]:ml-[100px] @[800px]:mr-[100px] @[800px]:mt-0 z-20">
+          <div className="grid @[900px]:grid-rows-3 z-20 gap-2 ">
+            <p className="text-[65px] text-center @[650px]:text-start font-extrabold ">
+              ADSSCODE
+            </p>
 
-				<div className="ml-[50px] mr-[50px] mt-[40px] @[800px]:ml-[100px] @[800px]:mr-[100px] @[800px]:mt-0 z-20">
-					<div className="grid @[900px]:grid-rows-3 z-20 gap-2 ">
-						<p className="text-[65px] text-center @[650px]:text-start font-extrabold ">
-							ADSSCODE
-						</p>
+            <p className="text-wrap  text-[20px] ">
+              Увеличиваем долю рынка, улучшаем сервис, повышаем лояльность и
+              продажи, снижаем расходы за счет автоматизации бизнес-процессов с
+              помощью мобильных приложений и веб-сервисов
+            </p>
 
-						<p className="text-wrap  text-[20px] ">
-							Увеличиваем долю рынка, улучшаем сервис, повышаем лояльность и продажи,
-							снижаем расходы за счет автоматизации бизнес-процессов с помощью
-							мобильных приложений и веб-сервисов
-						</p>
-
-						<Button
-							className="bg-[#A8ABAF]  hover:bg-[#BCBCBC] text-[23px] h-[60px]"
-							onClick={() => {
-								contactsRef.current?.scrollIntoView({
-									behavior: "smooth",
-								});
-							}}
-						>
-							Оставить заявку
-						</Button>
-					</div>
-				</div>
-				<img
-					src="/disabled_phone.png"
-					height={300}
-					width={300}
-					className="z-20 mr-[100px] hidden @[800px]:inline-flex"
-				/>
-			</div>
-		</div>
-	);
+            <Button
+              className="bg-[#A8ABAF]  hover:bg-[#BCBCBC] text-[23px] h-[60px]"
+              onClick={() => {
+                contactsRef.current?.scrollIntoView({
+                  behavior: "smooth",
+                });
+              }}
+            >
+              Оставить заявку
+            </Button>
+          </div>
+        </div>
+        <img
+          src="/disabled_phone.png"
+          height={300}
+          width={300}
+          className="z-20 mr-[100px] hidden @[800px]:inline-flex"
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,124 +9,124 @@ import Сontacts from "./Сontacts";
 import { OrderDialog } from "./OrderDialog";
 
 export interface BurgerProps {
-	aboutRef: React.MutableRefObject<HTMLDivElement | null>;
-	serviceRef: React.MutableRefObject<HTMLDivElement | null>;
-	portfolioRef: React.MutableRefObject<HTMLDivElement | null>;
-	contactsRef: React.MutableRefObject<HTMLDivElement | null>;
+  aboutRef: React.MutableRefObject<HTMLDivElement | null>;
+  serviceRef: React.MutableRefObject<HTMLDivElement | null>;
+  portfolioRef: React.MutableRefObject<HTMLDivElement | null>;
+  contactsRef: React.MutableRefObject<HTMLDivElement | null>;
 }
 
 export default function Layout() {
-	const aboutRef = useRef<HTMLDivElement | null>(null);
-	const serviceRef = useRef<HTMLDivElement | null>(null);
-	const portfolioRef = useRef<HTMLDivElement | null>(null);
-	const contactsRef = useRef<HTMLDivElement | null>(null);
+  const aboutRef = useRef<HTMLDivElement | null>(null);
+  const serviceRef = useRef<HTMLDivElement | null>(null);
+  const portfolioRef = useRef<HTMLDivElement | null>(null);
+  const contactsRef = useRef<HTMLDivElement | null>(null);
 
-	const props: BurgerProps = {
-		aboutRef,
-		serviceRef,
-		portfolioRef,
-		contactsRef,
-	};
+  const props: BurgerProps = {
+    aboutRef,
+    serviceRef,
+    portfolioRef,
+    contactsRef,
+  };
 
-	return (
-		<div className="flex flex-col min-h-screen bg-[#36353A]  ">
-			<header className="z-30 sticky top-0 bg-[#65646A] p-2  text-white @container">
-				<div className="items-center @[800px]:ml-[100px] @[800px]:mr-[100px] @[800px]:mt-0 z-20 ">
-					<div className="h-full float-start flex justify-start items-center">
-						<div className="flex justify-center items-center @[1200px]:hidden  font-bold mr-5">
-							<div className="">
-								<Burger links={props} />
-							</div>
-						</div>
-						<a
-							href="/"
-							className="hidden @[950px]:inline-flex text-[38px] ml-1 font-extrabold mr-3"
-						>
-							ADSSCODE
-						</a>
-						<div className=" mr-auto  hidden @[1200px]:inline-flex">
-							<Button
-								className="text-[17px] mr-4 bg-transparent  hidden @[1200px]:inline-flex hover:bg-gray-400"
-								onClick={() => {
-									aboutRef.current?.scrollIntoView({
-										behavior: "smooth",
-									});
-								}}
-							>
-								О нас
-							</Button>
-							<Button
-								className="text-[17px] mr-4 bg-transparent hover:bg-gray-400"
-								onClick={() => {
-									serviceRef.current?.scrollIntoView({
-										behavior: "smooth",
-									});
-								}}
-							>
-								Наши услуги
-							</Button>
-							<Button
-								className="text-[17px] mr-4 bg-transparent hover:bg-gray-400"
-								onClick={() => {
-									portfolioRef.current?.scrollIntoView({
-										behavior: "smooth",
-									});
-								}}
-							>
-								Портфолио
-							</Button>
-							<Button
-								className="text-[17px] bg-transparent hover:bg-gray-400"
-								onClick={() => {
-									contactsRef.current?.scrollIntoView({
-										behavior: "smooth",
-									});
-								}}
-							>
-								Контакты
-							</Button>
-						</div>
-					</div>
-					<div className="flex items-center float-right ">
-						<p className="mr-5 ">
-							Связаться с нами
-							<br />
-							<a href="tel:+7(901)350-77-86">+7(901)350-77-86</a>
-						</p>
-						<OrderDialog />
-					</div>
-				</div>
-			</header>
-			<main
-				className="min-w-full flex-grow   gap-4 mb-6"
-				onClick={(event: React.SyntheticEvent) => {
-					event.preventDefault();
-					const target = event.target as HTMLAnchorElement;
-					const id = target.getAttribute("href")?.replace("#", "");
-					const element = document.getElementById(String(id));
-					element?.scrollIntoView({
-						behavior: "instant",
-					});
-				}}
-			>
-				<div id="about" ref={aboutRef}>
-					<About contactsRef={contactsRef} />
-				</div>
+  return (
+    <div className="flex flex-col min-h-screen bg-[#36353A] overflow-x-hidden">
+      <header className="z-30 sticky top-0 bg-[#65646A] p-2  text-white @container">
+        <div className="items-center @[800px]:ml-[100px] @[800px]:mr-[100px] @[800px]:mt-0 z-20 ">
+          <div className="h-full float-start flex justify-start items-center">
+            <div className="flex justify-center items-center @[1200px]:hidden  font-bold mr-5">
+              <div className="">
+                <Burger links={props} />
+              </div>
+            </div>
+            <a
+              href="/"
+              className="hidden @[950px]:inline-flex text-[38px] ml-1 font-extrabold mr-3"
+            >
+              ADSSCODE
+            </a>
+            <div className=" mr-auto  hidden @[1200px]:inline-flex">
+              <Button
+                className="text-[17px] mr-4 bg-transparent  hidden @[1200px]:inline-flex hover:bg-gray-400"
+                onClick={() => {
+                  aboutRef.current?.scrollIntoView({
+                    behavior: "smooth",
+                  });
+                }}
+              >
+                О нас
+              </Button>
+              <Button
+                className="text-[17px] mr-4 bg-transparent hover:bg-gray-400"
+                onClick={() => {
+                  serviceRef.current?.scrollIntoView({
+                    behavior: "smooth",
+                  });
+                }}
+              >
+                Наши услуги
+              </Button>
+              <Button
+                className="text-[17px] mr-4 bg-transparent hover:bg-gray-400"
+                onClick={() => {
+                  portfolioRef.current?.scrollIntoView({
+                    behavior: "smooth",
+                  });
+                }}
+              >
+                Портфолио
+              </Button>
+              <Button
+                className="text-[17px] bg-transparent hover:bg-gray-400"
+                onClick={() => {
+                  contactsRef.current?.scrollIntoView({
+                    behavior: "smooth",
+                  });
+                }}
+              >
+                Контакты
+              </Button>
+            </div>
+          </div>
+          <div className="flex items-center float-right ">
+            <p className="mr-5 ">
+              Связаться с нами
+              <br />
+              <a href="tel:+7(901)350-77-86">+7(901)350-77-86</a>
+            </p>
+            <OrderDialog />
+          </div>
+        </div>
+      </header>
+      <main
+        className="min-w-full flex-grow gap-4 mb-6"
+        onClick={(event: React.SyntheticEvent) => {
+          event.preventDefault();
+          const target = event.target as HTMLAnchorElement;
+          const id = target.getAttribute("href")?.replace("#", "");
+          const element = document.getElementById(String(id));
+          element?.scrollIntoView({
+            behavior: "instant",
+          });
+        }}
+      >
+        <div id="about" ref={aboutRef}>
+          <About contactsRef={contactsRef} />
+        </div>
 
-				<div ref={serviceRef} id="service" className="scroll-mt-[70px]">
-					<Service contactsRef={contactsRef} />
-				</div>
-				<div ref={portfolioRef} id="portfolio" className="scroll-mt-[70px] ">
-					<Portfolio />
-				</div>
+        <div ref={serviceRef} id="service" className="scroll-mt-[70px]">
+          <Service contactsRef={contactsRef} />
+        </div>
+        <div ref={portfolioRef} id="portfolio" className="scroll-mt-[70px] ">
+          <Portfolio />
+        </div>
 
-				<div ref={contactsRef} id="contacts" className="scroll-mt-[70px] ">
-					<Сontacts />
-				</div>
-			</main>
-			<footer className="w-full row-auto">
-				<Footer />
-			</footer>
-		</div>
-	);
+        <div ref={contactsRef} id="contacts" className="scroll-mt-[70px] ">
+          <Сontacts />
+        </div>
+      </main>
+      <footer className="w-full row-auto">
+        <Footer />
+      </footer>
+    </div>
+  );
 }
